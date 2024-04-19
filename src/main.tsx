ba7a9d1ad96699ffe19ff31659391159ac5d5f46
snaztoz <hafidhmn@gmail.com>
2024-04-19 21:41:22 +0700

    feat: :tada: init project

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
